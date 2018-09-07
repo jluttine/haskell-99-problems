625e41e233885165c84e731ae22ac5fa853eea72
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-09-07 18:30:56 +0300

    Add solution to 1

---
+++ ex01.hs
@@ -0,0 +1,7 @@
+module Ex01 where
+
+-- |Find the last element of a list.
+myLast :: [a] -> a
+myLast (x : []) = x
+myLast (_ : xs) = myLast xs
+myLast [] = error "can't find the last element of an empty list"
