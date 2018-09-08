3f0adbbbd6838bb63a11e73cf036ad50c7cf8b58
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-09-08 10:22:22 +0300

    Add faster solution to 4

--- ex04.hs
+++ ex04.hs
@@ -1,9 +1,15 @@
+import Data.List
+
 -- |Find the number of elements of a list.
 myLength :: Integral n => [a] -> n
 myLength xs = run xs 0
   where
     run [] n = n
     run (_:xs) n = run xs (n+1)
+
+-- This is how it is implemented in Prelude and it's faster than the one above
+myLength' :: Integral n => [a] -> n
+myLength' = foldl' (\c _ -> c+1) 0
 
 -- Just for comparison, here's a bad solution which doesn't use tail recursion,
 -- thus leading to stack overflow on long lists.
