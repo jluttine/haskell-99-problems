10e2ffeebe246d31798f9b174ce3f05fdb5671dd
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-09-08 10:14:06 +0300

    Add solution to 4

---
+++ ex04.hs
@@ -0,0 +1,12 @@
+-- |Find the number of elements of a list.
+myLength :: Integral n => [a] -> n
+myLength xs = run xs 0
+  where
+    run [] n = n
+    run (_:xs) n = run xs (n+1)
+
+-- Just for comparison, here's a bad solution which doesn't use tail recursion,
+-- thus leading to stack overflow on long lists.
+myLengthBad :: Integral n => [a] -> n
+myLengthBad [] = 0
+myLengthBad (_:xs) = 1 + (myLengthBad xs)
