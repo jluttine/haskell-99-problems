1047ab1971c33263c8ea94c4cc8d95677e576015
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-09-08 09:45:45 +0300

    Fix solution to 2

--- ex02.hs
+++ ex02.hs
@@ -1,6 +1,6 @@
 -- |Find the last but one element of a list.
 myButLast :: [a] -> a
 myButLast (x : _ : []) = x
-myButLast (_ : _ : xs) = myButLast xs
 myButLast (_ : []) = error "can't find the last but one element of a list with one element"
+myButLast (_ : xs) = myButLast xs
 myButLast [] = error "can't find the last but one element of an empty list"
